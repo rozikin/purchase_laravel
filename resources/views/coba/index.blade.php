67443bdb8502f6fbfb114b6f1c017e04afdab18b
rozikin <khoirurrozikin86@gmail.com>
2022-10-18 13:08:41 +0700

    This is a new commit for what I originally planned to be amended

---
+++ resources/views/coba/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('title')
+    {{ $title }}
+@endsection
+
+@section('title_header')
+    {{ $title }}
+@endsection
+
+@section('content-body')
+   
+hahahah hahaha hehehehe
+    
+@endsection
